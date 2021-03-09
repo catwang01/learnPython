635e9d93ed8b5f863f6864591e900995613151eb
catwang01 <edwardelricwzx@gmail.com>
2021-03-09 21:47:46 +0800

    add files

---
+++ python-argparse/test.py
@@ -0,0 +1,20 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Unsupported value encountered.')
+
+parser.add_argument(
+    '--flag',
+    type=str2bool,
+    help='Turn on or turn off flag'
+)
+
+args = parser.parse_args()
+print(args.flag)
---
+++ python-import/test_code3/main.py
@@ -0,0 +1,1 @@
+from subfolder import demo
